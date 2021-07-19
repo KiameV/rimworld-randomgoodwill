5b98a282b43df20ca8156404588f8ba6b0627ebe
Travis Offtermatt <travisty2@gmail.com>
2021-07-19 17:09:47 -0600

    initial

---
+++ Source/HarmonyPatches.cs
@@ -0,0 +1,174 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace RandomGoodwill
+{
+    [StaticConstructorOnStartup]
+    internal static class Main
+    {
+        internal static bool IsConfigMapsLoaded = false;
+        static Main()
+        {
+            var harmony = new Harmony("com.rimworld.mod.factioncontrol");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            IsConfigMapsLoaded = ModLister.GetActiveModWithIdentifier("configurablemaps.kv.rw") != null;
+        }
+    }
+
+    [HarmonyPatch(typeof(Page_CreateWorldParams), "DoWindowContents")]
+    public static class Patch_Page_CreateWorldParams_DoWindowContents
+    {
+        static void Postfix(Rect rect)
+        {
+            float y = rect.y + rect.height - 78f;
+            Text.Font = GameFont.Small;
+            string label = "RandomGoodwill".Translate();
+            float x = 0f;
+            if (Main.IsConfigMapsLoaded)
+                x += 170;
+            if (Widgets.ButtonText(new Rect(x, y, 150, 32), label))
+            {
+                if (!Find.WindowStack.TryRemove(typeof(SettingsWindow)))
+                {
+                    Find.WindowStack.Add(new SettingsWindow());
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(LoadedModManager), "GetSettingsFilename", null)]
+    public static class LoadedModManager_GetSettingsFilename
+    {
+        private static void Prefix(string modIdentifier, string modHandleName, ref string __result)
+        {
+            if (modHandleName.Contains("Controller_ModdedFactions"))
+            {
+                modHandleName = "Controller_ModdedFactions";
+            }
+            __result = Path.Combine(GenFilePaths.ConfigFolderPath, GenText.SanitizeFilename(string.Format("Mod_{0}_{1}.xml", modIdentifier, modHandleName)));
+        }
+    }
+
+    [HarmonyPatch(typeof(Faction))]
+    [HarmonyPatch("Color", MethodType.Getter)]
+    [HarmonyPriority(Priority.Last)]
+    public static class Patch_Faction_get_Color
+    {
+        public static void Postfix(Faction __instance, ref Color __result)
+        {
+            if (!Settings.dynamicColors ||
+                __instance.def.isPlayer)
+            {
+                return;
+            }
+
+            if (__instance.HostileTo(Faction.OfPlayer))
+            {
+                float goodwill = GoodWillToColor(__instance.GoodwillWith(Faction.OfPlayer));
+                __result = new Color(0.75f, goodwill, goodwill);
+            }
+        }
+
+        private static float GoodWillToColor(int goodwill)
+        {
+            float v = Math.Abs(goodwill) * 0.01f;
+            //if (v > .65f)
+            //    v = .65f;
+            if (v > 1f)
+                v = 1f;
+            else if (v < 0.35f)
+                v = 0.35f;
+            v = 1 - v;
+            return v;
+        }
+    }
+
+    [HarmonyPatch(typeof(WorldGenerator), "GenerateWorld")]
+    public class WorldGenerator_Generate
+    {
+        public static void Postfix(World __result)
+        {
+            if (Settings.randomGoodwill && Settings.Factions.Count > 0)
+            {
+                foreach (var f in __result.factionManager.AllFactionsVisible)
+                {
+                    if (Settings.Factions.TryGetValue(f.def.defName, out FactionMinMax fmm))
+                    {
+                        FactionRelation fr = f.RelationWith(Faction.OfPlayer, true);
+                        if (fr != null)
+                        {
+                            int min = fmm.Min, max = fmm.Max;
+                            if (max < min)
+                            {
+                                Log.Warning($"[Random Goodwill] min is greater than max for {f.def.label} will flip the values.");
+                                var i = min;
+                                min = max;
+                                max = i;
+                            }
+                            fr.baseGoodwill = Rand.RangeInclusive(min, max);
+                            if (fr.baseGoodwill >= 0)
+                            {
+                                fr.kind = FactionRelationKind.Neutral;
+                            }
+                            else
+                            {
+                                fr.kind = FactionRelationKind.Hostile;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /*
+    [HarmonyPatch(typeof(Faction), "TryAffectGoodwillWith")]
+    public static class Patch_Faction_TryAffectGoodwillWith
+    {
+        static bool Prefix(Faction __instance, ref bool __result, Faction other, int goodwillChange, bool canSendMessage, bool canSendHostilityLetter, string reason, GlobalTargetInfo? lookTarget)
+        {
+            if (!Controller.Settings.relationsChangeOverTime)
+            {
+                if (other == Faction.OfPlayer &&
+                    goodwillChange < 0 &&
+                    !canSendMessage && !canSendHostilityLetter && reason == null && lookTarget == null)
+                {
+                    __result = false;
+                    return false;
+                }
+                if (reason != null && 
+                    __instance.def.naturalColonyGoodwill != null &&
+                    (reason.IndexOf("GoodwillChangedReason_NaturallyOverTime".Translate(__instance.def.naturalColonyGoodwill.min.ToString())) != -1 || 
+                     reason.IndexOf("GoodwillChangedReason_NaturallyOverTime".Translate(__instance.def.naturalColonyGoodwill.max.ToString())) != -1))
+                {
+
+                    __result = false;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(LetterStack), "ReceiveLetter", typeof(string), typeof(string), typeof(LetterDef), typeof(string))]
+    public static class Patch_LetterStack_ReceiveLetter
+    {
+        static bool Prefix(string label, string text, LetterDef textLetterDef, string debugInfo)
+        {
+            if (!Controller.Settings.relationsChangeOverTime && 
+                textLetterDef == LetterDefOf.NegativeEvent && 
+                label == "LetterLabelFactionBaseProximity".Translate())
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+    */
+}
---
+++ Source/Settings_Main.cs
@@ -0,0 +1,237 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace RandomGoodwill
+{
+    public class Controller : Mod
+    {
+        public Controller(ModContentPack content) : base(content)
+        {
+            base.GetSettings<Settings>();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "RandomGoodwill".Translate();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.GetSettings<Settings>().DoWindowContents(inRect);
+        }
+    }
+
+    public class FactionMinMax : IExposable
+    {
+        public FactionDef Faction;
+        public string defName;
+        public int Min;
+        public int Max;
+
+        public void ExposeData()
+        {
+            Scribe_Values.Look(ref defName, "defName");
+            Scribe_Values.Look(ref Min, "min");
+            Scribe_Values.Look(ref Max, "max");
+        }
+    }
+
+    public class Settings : ModSettings
+    {
+        public static bool randomGoodwill = true;
+        public static bool dynamicColors = true;
+
+        public static Dictionary<string, FactionMinMax> Factions = new Dictionary<string, FactionMinMax>();
+        public static bool createdFactions = false, populatedFactions = false;
+
+        struct Buffers
+        {
+            public string Min, Max;
+            public Buffers(string min, string max)
+            {
+                this.Min = min;
+                this.Max = max;
+            }
+        }
+        private string[] buffers;
+
+        public void DoWindowContents(Rect rect)
+        {
+            string min = "min".Translate().CapitalizeFirst();
+            string max = "max".Translate().CapitalizeFirst();
+            CreateFactions();
+            PopulateFactions();
+            if (buffers == null || buffers.Length != Factions.Count * 2)
+            {
+                UpdateBuffers();
+            }
+            float half = rect.width * 0.5f;
+            float width = 250f;
+
+            Listing_Standard list = new Listing_Standard { ColumnWidth = width};
+            list.Begin(new Rect(rect.x, rect.y, width, rect.height));
+            list.Gap(24);
+            list.CheckboxLabeled("RG.EnableFactionDynamicColors".Translate(), ref dynamicColors, "RFC.EnableFactionDynamicColorsTip".Translate());
+            list.Gap(24);
+            list.CheckboxLabeled("RG.EnableFactionRandomGoodwill".Translate(), ref randomGoodwill, "RFC.EnableFactionRandomGoodwillToolTip".Translate());
+            list.End();
+
+            if (randomGoodwill)
+            {
+                Widgets.Label(new Rect(half, rect.y, width, 28), "RG.GoodwillMinMax".Translate());
+                list = new Listing_Standard { ColumnWidth = width };
+                list.Begin(new Rect(half, rect.y + 34, width, rect.height));
+                int i = 0;
+                foreach (var f in Factions.Values)
+                {
+                    if (f.Faction != null)
+                    {
+                        list.Label(f.Faction.LabelCap);
+                        list.TextFieldNumericLabeled(min, ref f.Min, ref buffers[i], -100, 100);
+                        list.TextFieldNumericLabeled(max, ref f.Max, ref buffers[i + 1], -100, 100);
+                        if (list.ButtonText("RG.Default".Translate()))
+                        {
+                            var gw = GetInitialGoodwill(f.Faction);
+                            f.Min = gw;
+                            f.Max = gw;
+                            buffers[i] = gw.ToString();
+                            buffers[i + 1] = gw.ToString();
+                        }
+                        list.GapLine(6);
+                        i += 2;
+                    }
+                }
+                list.End();
+            }
+        }
+
+        private List<FactionMinMax> fmm = null;
+
+        public override void ExposeData()
+        {
+            CreateFactions();
+
+            if (fmm == null)
+                fmm = new List<FactionMinMax>(Factions.Count);
+
+            if (Scribe.mode == LoadSaveMode.Saving)
+            {
+                foreach (var kv in Factions)
+                {
+                    kv.Value.defName = kv.Key;
+                    fmm.Add(kv.Value);
+                }
+            }
+
+            base.ExposeData();
+            Scribe_Values.Look(ref randomGoodwill, "RandomGoodwill", true);
+            Scribe_Values.Look(ref dynamicColors, "DynamicColors", true);
+
+            if (fmm != null)
+            {
+                Scribe_Collections.Look(ref fmm, "factionGoodwill");
+            }
+
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                foreach (var f in fmm)
+                    Factions[f.defName] = f;
+            }
+
+            if (Scribe.mode == LoadSaveMode.Saving || Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                fmm?.Clear();
+                fmm = null;
+
+                UpdateBuffers();
+            }
+        }
+
+        private void UpdateBuffers()
+        {
+            if (buffers == null || buffers.Length != Factions.Count * 2)
+                buffers = new string[Factions.Count * 2];
+            int i = 0;
+            foreach (var f in Factions.Values)
+            {
+                buffers[i] = f.Min.ToString();
+                buffers[i + 1] = f.Max.ToString();
+                i += 2;
+            }
+        }
+
+        private void CreateFactions()
+        {
+            if (Factions == null)
+                Factions = new Dictionary<string, FactionMinMax>();
+
+            if (Factions.Count > 0 && createdFactions)
+                return;
+
+            foreach (var f in DefDatabase<FactionDef>.AllDefsListForReading)
+            {
+                if (f != null && !Factions.ContainsKey(f.defName))
+                {
+                    if (f == FactionDefOf.PlayerColony || f == FactionDefOf.PlayerTribe)
+                        continue;
+
+                    if (!f.hidden && !f.permanentEnemy)
+                    {
+                        var gw = GetInitialGoodwill(f);
+                        Factions.Add(f.defName, new FactionMinMax()
+                        {
+                            Min = gw,
+                            Max = gw
+                        });
+                    }
+                }
+            }
+            createdFactions = Factions.Count > 0;
+            if (createdFactions)
+                UpdateBuffers();
+        }
+
+        private void PopulateFactions()
+        {
+            if (!populatedFactions)
+            {
+                foreach (var f in Factions.Values)
+                {
+                    if (f.defName != null && f.defName != "")
+                    {
+                        var def = DefDatabase<FactionDef>.GetNamed(f.defName, false);
+                        if (def != null)
+                        {
+                            f.Faction = def;
+                        }
+                        else
+                        {
+                            Log.Warning($"[Random Goodwill] failed to load faction {f.defName}");
+                        }
+                    }
+                }
+                populatedFactions = true;
+            }
+        }
+
+        private static int GetInitialGoodwill(FactionDef a)
+        {
+            if (a.permanentEnemy)
+            {
+                return -100;
+            }
+            if (a.permanentEnemyToEveryoneExcept != null && !(a.permanentEnemyToEveryoneExcept.Contains(FactionDefOf.PlayerTribe) || a.permanentEnemyToEveryoneExcept.Contains(FactionDefOf.PlayerColony)))
+            {
+                return -100;
+            }
+            if (a.naturalEnemy)
+            {
+                return -80;
+            }
+            return 0;
+        }
+    }
+}
